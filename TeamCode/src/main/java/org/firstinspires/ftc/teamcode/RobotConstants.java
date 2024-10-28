9d2021e267af37f22f2474dca4431e901b8ee0b9
Blaziumm <eclipsium3@gmail.com>
2024-10-28 10:46:46 -0400

    Initial Commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotConstants.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class RobotConstants {
+    public static int MAGIC_NUMBER = 32;
+    public static Double VericalSlideP = (double) 0;
+    public static Double VericalSlideI = (double) 0;
+    public static Double VericalSlideD = (double) 0;
+
+    public static Double horizontalSlideP = (double) 0;
+    public static Double horizontalSlideI = (double) 0;
+    public static Double horizontalSlideD = (double) 0;
+
+    // other constants
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import dev.frozenmilk.dairy.core.util.OpModeLazyCell;
+import dev.frozenmilk.dairy.core.util.features.BulkRead;
+
+// lets add that BulkRead feature from Writing + Using Features
+@BulkRead.Attach
+public abstract class RobotHardware extends OpMode {
+    // lets add OpModeLazyCells for each hardware item
+    private final OpModeLazyCell<DcMotorEx> leftFront
+            = new OpModeLazyCell<>(() -> hardwareMap.get(DcMotorEx.class, "lf"));
+    private final OpModeLazyCell<DcMotorEx> leftBack
+            = new OpModeLazyCell<>(() -> hardwareMap.get(DcMotorEx.class, "lb"));
+    // right motors need some additional setup
+    private final OpModeLazyCell<DcMotorEx> rightBack = new OpModeLazyCell<>(() -> {
+        DcMotorEx m = hardwareMap.get(DcMotorEx.class, "rb");
+        m.setDirection(DcMotorSimple.Direction.REVERSE);
+        return m;
+    });
+    private final OpModeLazyCell<DcMotorEx> rightFront = new OpModeLazyCell<>(() -> {
+        DcMotorEx m = hardwareMap.get(DcMotorEx.class, "rf");
+        m.setDirection(DcMotorSimple.Direction.REVERSE);
+        return m;
+    });
+
+    // and lets add getters to access the cells
+    public DcMotorEx getLeftFront() { return leftFront.get(); }
+    public DcMotorEx getLeftBack() { return leftBack.get(); }
+    public DcMotorEx getRightBack() { return rightBack.get(); }
+    public DcMotorEx getRightFront() { return rightFront.get(); }
+}
