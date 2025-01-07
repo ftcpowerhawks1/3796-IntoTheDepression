73b3a73cef6940038b2edd370fbbe9e81601726f
AlmostAwake <80174486+Almost-Awake@users.noreply.github.com>
2025-01-06 20:07:50 -0500

    update pid values for vertical slides

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/VerticalSlidesPIDConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/VerticalSlidesPIDConfig.java
@@ -5,7 +5,7 @@ import com.acmerobotics.dashboard.config.Config;
 @Config
 public class VerticalSlidesPIDConfig {
     //PID Values
-    public static double SlidesP = 0.0001;
+    public static double SlidesP = 0.0025;
     public static double SlidesI = 0;
-    public static double SlidesD = 0.0025;
+    public static double SlidesD = 0.0001;
 }
