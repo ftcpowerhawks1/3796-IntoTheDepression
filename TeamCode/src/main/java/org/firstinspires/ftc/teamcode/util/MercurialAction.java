a288941e258e1ab5d36d735ffdbe2e0699b517cf
Evan <eclipsium10@gmail.com>
2025-01-08 18:46:18 -0500

    feat: Create converter from Mercurial to Roadrunner Actions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/MercurialAction.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.util;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+
+import dev.frozenmilk.mercurial.Mercurial;
+import dev.frozenmilk.mercurial.commands.Command;
+
+public class MercurialAction implements Action {
+    private final Command command;
+    private boolean initialized = false;
+    public MercurialAction(Command command) {
+        this.command = command;
+    }
+
+    @Override
+    public boolean run(@NonNull TelemetryPacket packet) {
+        final boolean initialized = this.initialized;
+        if (!initialized) {
+            command.schedule();
+            this.initialized = true;
+        }
+        final boolean finished = initialized && !Mercurial.isScheduled(command);
+        if (finished) this.initialized = false;
+        return finished;
+    }
+}
