afb8cbf71cb150617e1061a95249a311742f6bad
Evan <eclipsium10@gmail.com>
2025-01-17 23:34:16 -0500

    Revert "fix: please for the sake of everything work"
    This reverts commit 040a5196077cff04997822319225f02ba6ccf761.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto.java
@@ -15,101 +15,85 @@ import org.firstinspires.ftc.teamcode.util.SilkRoad;
 import org.firstinspires.ftc.teamcode.util.talonsOpMode;
 
 import dev.frozenmilk.mercurial.commands.groups.Parallel;
-import dev.frozenmilk.mercurial.commands.groups.Sequential;
-import dev.frozenmilk.mercurial.commands.util.Wait;
 
 @SilkRoad.Attach
 @Autonomous(name = "Auto", preselectTeleOp = "Simple OpMode")
 public class auto extends talonsOpMode {
     Pose2d initialPose = new Pose2d(-35, -61,Math.toRadians(270));
-    Action mainAction;
-    MecanumDrive drive;
+    MecanumDrive drive = new MecanumDrive(hardwareMap,initialPose);
+    Action mainAction = drive.actionBuilder(initialPose)
+            //Specimen
+            .stopAndAdd(new MercurialAction(VerticalSlides.INSTANCE.setSlidePosition(VerticalSlides.SlideState.SPECIMEN_SCORING)))
 
+            .setReversed(true)
+                    .splineToConstantHeading(new Vector2d(-9,-33),Math.toRadians(90))
+            .stopAndAdd(new MercurialAction(VerticalSlides.INSTANCE.setSlidePosition(VerticalSlides.SlideState.HOME)))
 
-    @Override
-    public void init() {
-        drive = new MecanumDrive(hardwareMap,initialPose);
-        mainAction = drive.actionBuilder(initialPose)
-                //Specimen
-                .stopAndAdd(new MercurialAction(VerticalSlides.INSTANCE.setSlidePosition(VerticalSlides.SlideState.SPECIMEN_SCORING)))
-
-                .setReversed(true)
-                .splineToConstantHeading(new Vector2d(-9,-33),Math.toRadians(90))
-                .stopAndAdd(new MercurialAction(VerticalSlides.INSTANCE.setSlidePosition(VerticalSlides.SlideState.HOME)))
-
-                .setReversed(false)
-                //First Sample
+            .setReversed(false)
+            //First Sample
                 .splineToSplineHeading(new Pose2d(-52,-48,Math.toRadians(135)),Math.toRadians(180))
-                .stopAndAdd(new MercurialAction(new Sequential(
-                        new Parallel(
-                                HorizontalSlides.INSTANCE.setSlidePosition(HorizontalSlides.SlideState.FULL_EXTEND),
-                                Intake.INSTANCE.setIntakePosition(Intake.IntakeState.INTAKING)
-                        ), new Wait(1),
-                        new Parallel(
-                                HorizontalSlides.INSTANCE.setSlidePosition(HorizontalSlides.SlideState.HOME),
-                                Intake.INSTANCE.setIntakePosition(Intake.IntakeState.RETRACTED).then(Intake.INSTANCE.intake(0)))
+
+            .stopAndAdd(new MercurialAction(new Parallel(
+                    HorizontalSlides.INSTANCE.setSlidePosition(HorizontalSlides.SlideState.FULL_EXTEND),
+                    Intake.INSTANCE.setIntakePosition(Intake.IntakeState.INTAKING))))
+
+            .waitSeconds(1)
+
+            .stopAndAdd(new MercurialAction(new Parallel(
+                    HorizontalSlides.INSTANCE.setSlidePosition(HorizontalSlides.SlideState.HOME),
+                    Intake.INSTANCE.setIntakePosition(Intake.IntakeState.RETRACTED).then(Intake.INSTANCE.intake(0)))))
+
+            .splineToLinearHeading(new Pose2d(-55,-55,Math.toRadians(45)),Math.toRadians(90))
+
+            .stopAndAdd(new MercurialAction(VerticalSlides.INSTANCE.setSlidePosition(VerticalSlides.SlideState.HIGH_SCORING)))
+            .stopAndAdd(new MercurialAction(Outtake.INSTANCE.setOuttakePivot(Outtake.OuttakePivotState.UP)))
+
+            .waitSeconds(0.5)
+
+            .stopAndAdd(new MercurialAction(new Parallel(
+                    VerticalSlides.INSTANCE.setSlidePosition(VerticalSlides.SlideState.HOME),
+                    Outtake.INSTANCE.setOuttakePivot(Outtake.OuttakePivotState.DOWN))))
 
 
-                )))
+            //Second Sample
+            .splineToLinearHeading(new Pose2d(-58,-43,Math.toRadians(90)),Math.toRadians(90))
+            .stopAndAdd(new MercurialAction(new Parallel(
+                    HorizontalSlides.INSTANCE.setSlidePosition(HorizontalSlides.SlideState.FULL_EXTEND),
+                    Intake.INSTANCE.setIntakePosition(Intake.IntakeState.INTAKING))))
 
-                .waitSeconds(1)
+            .waitSeconds(1)
 
+            .stopAndAdd(new MercurialAction(new Parallel(
+                    HorizontalSlides.INSTANCE.setSlidePosition(HorizontalSlides.SlideState.HOME),
+                    Intake.INSTANCE.setIntakePosition(Intake.IntakeState.RETRACTED).then(Intake.INSTANCE.intake(0)))))
+            .splineToLinearHeading(new Pose2d(-55,-55,Math.toRadians(45)),Math.toRadians(90))
 
-                .splineToLinearHeading(new Pose2d(-55,-55,Math.toRadians(45)),Math.toRadians(90))
+            .stopAndAdd(new MercurialAction(VerticalSlides.INSTANCE.setSlidePosition(VerticalSlides.SlideState.HIGH_SCORING)))
+            .stopAndAdd(new MercurialAction(Outtake.INSTANCE.setOuttakePivot(Outtake.OuttakePivotState.UP)))
+            .waitSeconds(0.5)
+            .stopAndAdd(new MercurialAction(Outtake.INSTANCE.setOuttakePivot(Outtake.OuttakePivotState.DOWN)))
+            .stopAndAdd(new MercurialAction(VerticalSlides.INSTANCE.setSlidePosition(VerticalSlides.SlideState.HOME)))
 
-                .stopAndAdd(new MercurialAction(VerticalSlides.INSTANCE.setSlidePosition(VerticalSlides.SlideState.HIGH_SCORING)))
-                .stopAndAdd(new MercurialAction(Outtake.INSTANCE.setOuttakePivot(Outtake.OuttakePivotState.UP)))
+            //Third Sample
+            .splineToLinearHeading(new Pose2d(-58,-43,Math.toRadians(60)),Math.toRadians(90))
+            .stopAndAdd(new MercurialAction(new Parallel(
+                    HorizontalSlides.INSTANCE.setSlidePosition(HorizontalSlides.SlideState.FULL_EXTEND),
+                    Intake.INSTANCE.setIntakePosition(Intake.IntakeState.INTAKING))))
 
-                .waitSeconds(0.5)
+            .waitSeconds(1)
 
-                .stopAndAdd(new MercurialAction(new Parallel(
-                        VerticalSlides.INSTANCE.setSlidePosition(VerticalSlides.SlideState.HOME),
-                        Outtake.INSTANCE.setOuttakePivot(Outtake.OuttakePivotState.DOWN))))
+            .stopAndAdd(new MercurialAction(new Parallel(
+                    HorizontalSlides.INSTANCE.setSlidePosition(HorizontalSlides.SlideState.HOME),
+                    Intake.INSTANCE.setIntakePosition(Intake.IntakeState.RETRACTED).then(Intake.INSTANCE.intake(0)))))
+            .splineToLinearHeading(new Pose2d(-55,-55,Math.toRadians(45)),Math.toRadians(90))
 
+            .stopAndAdd(new MercurialAction(VerticalSlides.INSTANCE.setSlidePosition(VerticalSlides.SlideState.HIGH_SCORING)))
+            .stopAndAdd(new MercurialAction(Outtake.INSTANCE.setOuttakePivot(Outtake.OuttakePivotState.UP)))
+            .waitSeconds(0.5)
+            .stopAndAdd(new MercurialAction(Outtake.INSTANCE.setOuttakePivot(Outtake.OuttakePivotState.DOWN)))
+            .stopAndAdd(new MercurialAction(VerticalSlides.INSTANCE.setSlidePosition(VerticalSlides.SlideState.HOME)))
 
-                //Second Sample
-                .splineToLinearHeading(new Pose2d(-58,-43,Math.toRadians(90)),Math.toRadians(90))
-                .stopAndAdd(new MercurialAction(new Sequential(
-                        new Parallel(
-                                HorizontalSlides.INSTANCE.setSlidePosition(HorizontalSlides.SlideState.FULL_EXTEND),
-                                Intake.INSTANCE.setIntakePosition(Intake.IntakeState.INTAKING)
-                        ), new Wait(1),
-                        new Parallel(
-                                HorizontalSlides.INSTANCE.setSlidePosition(HorizontalSlides.SlideState.HOME),
-                                Intake.INSTANCE.setIntakePosition(Intake.IntakeState.RETRACTED).then(Intake.INSTANCE.intake(0)))
-
-
-                )))
-                .splineToLinearHeading(new Pose2d(-55,-55,Math.toRadians(45)),Math.toRadians(90))
-
-                .stopAndAdd(new MercurialAction(VerticalSlides.INSTANCE.setSlidePosition(VerticalSlides.SlideState.HIGH_SCORING)))
-                .stopAndAdd(new MercurialAction(Outtake.INSTANCE.setOuttakePivot(Outtake.OuttakePivotState.UP)))
-                .waitSeconds(0.5)
-                .stopAndAdd(new MercurialAction(Outtake.INSTANCE.setOuttakePivot(Outtake.OuttakePivotState.DOWN)))
-                .stopAndAdd(new MercurialAction(VerticalSlides.INSTANCE.setSlidePosition(VerticalSlides.SlideState.HOME)))
-
-                //Third Sample
-                .splineToLinearHeading(new Pose2d(-58,-43,Math.toRadians(60)),Math.toRadians(90))
-                .stopAndAdd(new MercurialAction(new Sequential(
-                        new Parallel(
-                                HorizontalSlides.INSTANCE.setSlidePosition(HorizontalSlides.SlideState.FULL_EXTEND),
-                                Intake.INSTANCE.setIntakePosition(Intake.IntakeState.INTAKING)
-                        ), new Wait(1),
-                        new Parallel(
-                                HorizontalSlides.INSTANCE.setSlidePosition(HorizontalSlides.SlideState.HOME),
-                                Intake.INSTANCE.setIntakePosition(Intake.IntakeState.RETRACTED).then(Intake.INSTANCE.intake(0)))
-
-
-                )))
-                .splineToLinearHeading(new Pose2d(-55,-55,Math.toRadians(45)),Math.toRadians(90))
-
-                .stopAndAdd(new MercurialAction(VerticalSlides.INSTANCE.setSlidePosition(VerticalSlides.SlideState.HIGH_SCORING)))
-                .stopAndAdd(new MercurialAction(Outtake.INSTANCE.setOuttakePivot(Outtake.OuttakePivotState.UP)))
-                .waitSeconds(0.5)
-                .stopAndAdd(new MercurialAction(VerticalSlides.INSTANCE.setSlidePosition(VerticalSlides.SlideState.HOME)))
-                .stopAndAdd(new MercurialAction(Outtake.INSTANCE.setOuttakePivot(Outtake.OuttakePivotState.DOWN)))
-
-                //TODO Everything after this
+            //TODO Everything after this
 
 //            //Fourth Sample
 //            .splineToLinearHeading(new Pose2d(-25,-10,Math.toRadians(0)),Math.toRadians(0))
@@ -126,7 +110,11 @@ public class auto extends talonsOpMode {
 //            .stopAndAdd(new MercurialAction(VerticalSlides.INSTANCE.setSlidePosition(VerticalSlides.SlideState.HOME)))
 //
 
-                .build();
+            .build();
+
+    @Override
+    public void init() {
+
     }
 
     @Override
