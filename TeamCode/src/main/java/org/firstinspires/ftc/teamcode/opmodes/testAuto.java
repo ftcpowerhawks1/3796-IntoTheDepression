d162c0fe01281df10c91fe2b3c42372ab9154854
Evan <eclipsium10@gmail.com>
2025-01-17 23:07:27 -0500

    feat: add the test auto script

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testAuto.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
+import org.firstinspires.ftc.teamcode.subsystems.HorizontalSlides;
+import org.firstinspires.ftc.teamcode.subsystems.Intake;
+import org.firstinspires.ftc.teamcode.subsystems.Outtake;
+import org.firstinspires.ftc.teamcode.subsystems.VerticalSlides;
+import org.firstinspires.ftc.teamcode.util.SilkRoad;
+import org.firstinspires.ftc.teamcode.util.talonsOpMode;
+import org.firstinspires.ftc.teamcode.util.utilActions.MercurialAction;
+
+import dev.frozenmilk.mercurial.commands.groups.Parallel;
+
+@SilkRoad.Attach
+@Autonomous(name = "Test Auto", preselectTeleOp = "Simple OpMode")
+public class testAuto extends talonsOpMode {
+    Pose2d initialPose = new Pose2d(-35, -61,Math.toRadians(270));
+    MecanumDrive drive = new MecanumDrive(hardwareMap,initialPose);
+    Action mainAction = drive.actionBuilder(initialPose)
+            .stopAndAdd(new MercurialAction(VerticalSlides.INSTANCE.setSlidePosition(VerticalSlides.SlideState.SPECIMEN_SCORING)))
+            .build();
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void start() {
+        SilkRoad.RunAsync(
+                mainAction
+        );
+    }
+    @Override
+    public void loop() {
+
+    }
+}
